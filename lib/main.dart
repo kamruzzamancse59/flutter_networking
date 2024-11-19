7255d2de4cff3a2b6e0c83df1a2d105360bd7fab
Md. Kamruzzaman <141571750+kamruzzamancse59@users.noreply.github.com>
2024-11-19 12:47:26 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/http_screen.dart';
-import 'screens/chopper_screen.dart';
+import 'package:flutter_networking/screens/http_screen.dart';
+import 'package:flutter_networking/screens/chopper_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,20 +11,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Networking with Flutter',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Networking Example')),
-        body: Column(
+      home: HomeScreen(), // Wrap the Scaffold in a separate widget
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Networking Example'),
+      ),
+      body: Center( // Center the buttons in the screen
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             ElevatedButton(
               child: Text('HTTP Example'),
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (_) => HttpScreen()));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HttpScreen()),
+                );
               },
             ),
+            SizedBox(height: 20), // Add space between buttons
             ElevatedButton(
               child: Text('Chopper Example'),
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (_) => ChopperScreen()));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChopperScreen()),
+                );
               },
             ),
           ],
