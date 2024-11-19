9e2830364a638fd142ffec70883d1035824aa737
Md. Kamruzzaman <141571750+kamruzzamancse59@users.noreply.github.com>
2024-11-19 13:17:00 +0600

    Update chopper_service.chopper.dart

--- lib/services/chopper_service.chopper.dart
+++ lib/services/chopper_service.chopper.dart
@@ -1,12 +1,8 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
+
 
 part of 'chopper_service.dart';
 
-// **************************************************************************
-// ChopperGenerator
-// **************************************************************************
 
-// ignore_for_file: always_put_control_body_on_new_line, always_specify_types, prefer_const_declarations, unnecessary_brace_in_string_interps
 class _$PostService extends PostService {
   _$PostService([ChopperClient? client]) {
     if (client == null) return;
