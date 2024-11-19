f6004a916291e62adbd49d51f0e75866f7d9ad9c
Md. Kamruzzaman <141571750+kamruzzamancse59@users.noreply.github.com>
2024-11-19 13:15:54 +0600

    Update chopper_service.dart

--- lib/services/chopper_service.dart
+++ lib/services/chopper_service.dart
@@ -10,10 +10,10 @@ abstract class PostService extends ChopperService {
   static PostService create() {
     final client = ChopperClient(
       baseUrl: 'https://jsonplaceholder.typicode.com',
-      services: [_$PostService()], // This is generated; ensure this is correct
+      services: [_$PostService()], 
       interceptors: [HttpLoggingInterceptor()],
       converter: JsonConverter(),
     );
-    return _$PostService(client); // This is also generated
+    return _$PostService(client); 
   }
 }
